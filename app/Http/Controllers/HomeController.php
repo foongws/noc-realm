6f7ccb05edc4c0e64df59c1af2056f88e672429c
steven.foong <steven.foong@orion.co.com>
2019-01-18 16:13:37 +0800

    fix home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('nocrealm\home');
+        return view('nocrealm.home');
 	#return view('login');
     }
 }
