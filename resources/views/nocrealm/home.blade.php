f22f2271cc95951afd7bc9ee010fe7279399e743
steven.foong <steven.foong@orion.co.com>
2019-01-18 17:30:57 +0800

    enchance widget

--- resources/views/nocrealm/home.blade.php
+++ resources/views/nocrealm/home.blade.php
@@ -3,7 +3,6 @@
 @section('content')
     <div class="page-content">
         @include('voyager::alerts')
-        @include('voyager::dimmers')
         @include('nocrealm.dimmers')
     </div>
 @stop
