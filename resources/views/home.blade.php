55765c18ed3b6482204c1fdbd457a30f410a81a8
steven.foong <steven.foong@orion.co.com>
2019-01-18 11:57:22 +0800

    Migrate

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}" @if (config('voyager.multilingual.rtl')) dir="rtl" @endif>
 <head>
-    <title>@yield('page_title', setting('site.title') . " - " . setting('site.description'))</title>
+    <title>@yield('page_title', setting('admin.title') . " - " . setting('admin.description'))</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
 
@@ -68,41 +68,8 @@ if (starts_with(Auth::user()->avatar, 'http://') || starts_with(Auth::user()->av
 <div class="app-container">
     <div class="fadetoblack visible-xs"></div>
     <div class="row content-container">
-        @include('voyager::dashboard.navbar')
-	<div class="side-menu sidebar-inverse">
-	  <nav class="navbar navbar-default" role="navigation">
-            <div class="side-menu-container">
-              <div class="navbar-header">
-                <a class="navbar-brand" href="{{ route('voyager.dashboard') }}">
-                  <div class="logo-icon-container">
-                    <?php $admin_logo_img = Voyager::setting('site.login_icon_image', ''); ?>
-                        @if($admin_logo_img == '')
-                            <img src="{{ voyager_asset('images/logo-icon-light.png') }}" alt="Logo Icon">
-                        @else
-                            <img src="{{ Voyager::image($admin_logo_img) }}" alt="Logo Icon">
-                        @endif
-                  </div>
-                  <div class="title">{{Voyager::setting('site.title', 'VOYAGER')}}</div>
-                </a>
-              </div><!-- .navbar-header -->
-
-              <div class="panel widget center bgimage"
-                 style="background-image:url({{ Voyager::image( Voyager::setting('site.login_bg_image'), voyager_asset('images/bg.jpg') ) }}); background-size: cover; background-position: 0px;">
-                <div class="dimmer"></div>
-                <div class="panel-content">
-                  <img src="{{ $user_avatar }}" class="avatar" alt="{{ Auth::user()->name }} avatar">
-                    <h4>{{ ucwords(Auth::user()->name) }}</h4>
-                    <p>{{ Auth::user()->email }}</p>
-
-                    <a href="{{ route('voyager.profile') }}" class="btn btn-primary">{{ __('voyager::generic.profile') }}</a>
-                <div style="clear:both"></div>
-              </div>
-            </div>
-           </div>
-            {!! menu('user', 'admin_menu') !!}
-          </nav>
-        </div>
-
+        @include('navbar')
+        @include('sidebar')
         <script>
             (function(){
                     var appContainer = document.querySelector('.app-container'),
