35ec2f9588c481c4a4246d0808d5aa2bc0c82463
steven.foong <steven.foong@orion.co.com>
2019-01-18 18:43:59 +0800

    add profile

--- routes/web.php
+++ routes/web.php
@@ -45,3 +45,7 @@ Route::get('login', function(){
 Auth::routes(['register' => false]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/profile',function () {
+    return view('nocrealm.profile');
+}
